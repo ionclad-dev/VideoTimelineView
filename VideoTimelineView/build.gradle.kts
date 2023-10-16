47c75287aa93fadb7fc503f4cf0f23c4f646b35a
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2023-10-16 11:53:01 +0300

    bug fix

--- VideoTimelineView/build.gradle.kts
+++ VideoTimelineView/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "dev.iconclad.videotimelineview"
                 artifactId = "videotimelineview"
-                version = "0.0.4"
+                version = "0.0.5"
 
             }
         }
