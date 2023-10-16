c8c5ec24da468683e108d189c71e7b93c3ba495c
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2023-10-16 11:45:47 +0300

    bug fix

--- VideoTimelineView/build.gradle.kts
+++ VideoTimelineView/build.gradle.kts
@@ -30,14 +30,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
